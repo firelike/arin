79e6a6630d192c7bb7eb10fc1465433b53433efe
Plamen Garkov <plamen@acctbyte.com>
2014-02-07 18:05:19 -0800

    made some changes

---
+++ Request/AsnLookup.php
@@ -0,0 +1,63 @@
+<?php
+
+
+
+class AcctByte_Arin_Request_AsnLookup
+{
+
+    protected $method_name = 'asn';
+
+    private $handle;
+
+
+    public function __construct ($inRequestArguments)
+    {
+        if ( isset($inRequestArguments[ 'handle' ]) ) {
+            $this->setHandle($inRequestArguments[ 'handle' ]);
+        }
+    
+    }
+
+
+    /**
+     * @return the $command
+     */
+    public function getMethodName ()
+    {
+        return $this->method_name;
+    }
+
+
+    public function getQueryString ()
+    {
+        
+//        $args[ 'identifier' ] = $this->getIdentifier();
+//        $query_str = http_build_query($args);
+//        return $query_str;
+            
+        return $this->getHandle();
+        
+        
+    }
+    
+    
+	/**
+     * @return the $handle
+     */
+    public function getHandle ()
+    {
+        return $this->handle;
+    }
+
+	/**
+     * @param $handle the $handle to set
+     */
+    public function setHandle ($handle)
+    {
+        $this->handle = $handle;
+    }
+
+
+
+
+}
