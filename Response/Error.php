92cee1b6a82f7306466966231f8ca54bbad73321
Plamen Garkov <plamen@acctbyte.com>
2014-02-07 18:10:00 -0800

    once more

---
+++ Response/Error.php
@@ -0,0 +1,34 @@
+<?php
+class AcctByte_Arin_Response_Error
+{
+
+    protected $ErrorID;
+
+    protected $ErrorText;
+
+
+    public function __construct ($inErrorID, $inErrorText)
+    {
+        $this->ErrorID = $inErrorID;
+        $this->ErrorText = $inErrorText;
+    }
+
+
+    public function getErrorMessage ()
+    {
+        return 'Operation failed.More Info: <div>' . $this->ErrorText . ' [ Code ' . $this->ErrorID . ' ]</div>';
+    }
+
+
+    public function toArray ()
+    {
+        return array( 
+            'Error' => array( 
+                'ErrorID' => $this->ErrorID , 
+                'ErrorText' => $this->ErrorText 
+            ) 
+        );
+    }
+
+
+}
