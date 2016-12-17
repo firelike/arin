3a52f4a49621ccca419bead2f4f2ab8f76e8c051
Plamen Garkov <plamen@flexiebooks.com>
2016-12-17 10:06:58 -0800

    cleaning tests

---
+++ tests/Service/ArinServiceTest.php
@@ -0,0 +1,104 @@
+<?php
+namespace Firelike\ArinTest\Service;
+
+use Firelike\Arin\Service\ArinService;
+use PHPUnit\Framework\TestCase;
+
+
+/**
+ * ArinService test case.
+ */
+class ArinServiceTest extends TestCase
+{
+
+    /**
+     * @var ArinService
+     */
+    private $service;
+
+
+    /**
+     * Tests ArinService->AsnLookup()
+     */
+    public function testAsnLookup()
+    {
+        $this->markTestIncomplete("AsnLookup test not implemented");
+
+        $this->service->AsnLookup(/* parameters */);
+
+    }
+
+
+    /**
+     * Tests ArinService->CustomerLookup()
+     */
+    public function testCustomerLookup()
+    {
+        $this->markTestIncomplete("CustomerLookup test not implemented");
+
+        $this->service->CustomerLookup(/* parameters */);
+
+    }
+
+
+    /**
+     * Tests ArinService->IpLookup()
+     */
+    public function testIpLookup()
+    {
+        $this->markTestIncomplete("IpLookup test not implemented");
+
+        $this->service->IpLookup(/* parameters */);
+
+    }
+
+
+    /**
+     * Tests ArinService->NetLookup()
+     */
+    public function testNetLookup()
+    {
+        $this->markTestIncomplete("NetLookup test not implemented");
+
+        $this->service->NetLookup(/* parameters */);
+
+    }
+
+
+    /**
+     * Tests ArinService->OrgLookup()
+     */
+    public function testOrgLookup()
+    {
+        $this->markTestIncomplete("OrgLookup test not implemented");
+
+        $this->service->OrgLookup(/* parameters */);
+
+    }
+
+
+    /**
+     * Tests ArinService->PocLookup()
+     */
+    public function testPocLookup()
+    {
+        $this->markTestIncomplete("PocLookup test not implemented");
+
+        $this->service->PocLookup(/* parameters */);
+
+    }
+
+
+    /**
+     * Tests ArinService->apiCall()
+     */
+    public function testApiCall()
+    {
+        $this->markTestIncomplete("apiCall test not implemented");
+
+        $this->service->apiCall(/* parameters */);
+
+    }
+
+}
+
